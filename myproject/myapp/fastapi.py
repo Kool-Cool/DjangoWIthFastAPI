dda1db5b6ef1e6c46b67ff197a7aa3df365953fa
Kool-Cool <112835534+Kool-Cool@users.noreply.github.com>
2024-08-08 11:16:03 +0530

    added the Model
    ```
    python manage.py makemigrations
    ```

--- myproject/myapp/fastapi.py
+++ myproject/myapp/fastapi.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+# from .models import User as DjangoUser
+
 
 app = FastAPI()
 
@@ -7,3 +9,8 @@ def hello():
     return {"message" : "welcome from FastAPI"}
 
 
+# @app.get("/api/users")
+# def users():
+#     users = DjangoUser.objects.all()
+#     user_list = [{"id": user.id, "name": user.name} for user in users]
+#     return {"users": user_list}
